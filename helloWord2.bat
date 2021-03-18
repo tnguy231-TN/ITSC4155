5f70ce5078313f07b01243e8e663829aa5d5de42
tnguy231-TN <71344519+tnguy231-TN@users.noreply.github.com>
2021-03-18 00:04:29 -0400

    add helloWorld2

---
+++ helloWord2.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Hello World!
+echo My name is Thao Nguyen
+echo My major is computer science and concentration on Software Engineering
+echo My expected graduation is Fall 2021
+timeout 10
