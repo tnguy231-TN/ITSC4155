f34d535b42b8b563ec4883ee16c762ca28f7842a
tnguy231-TN <71344519+tnguy231-TN@users.noreply.github.com>
2021-03-17 23:57:52 -0400

    Delete helloWorld2.bat

--- helloWorld2.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Hello World!
-echo My name is Thao Nguyen.
-echo My major is computer science and concentration in software engineering
-echo My expected graduation is Fall 20210
-time out 15
